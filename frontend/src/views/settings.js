6c528109b5300949d98f2df78d65253515eb4930
Bryan Calisto <bryancalisto@yahoo.es>
2021-12-14 18:34:27 -0500

    Chore: Build prototype for speech to text use case

--- frontend/src/views/settings.js
+++ frontend/src/views/settings.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-
+import React, { useEffect } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 const SettingsView = () => {
-  const {
-    transcript,
-    listening,
-    resetTranscript,
-    browserSupportsSpeechRecognition
-  } = useSpeechRecognition();
+  const { transcript, listening, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
 
+  useEffect(() => {
+    SpeechRecognition.startListening({ continuous: true });
+    setInterval(() => {
+      SpeechRecognition.stopListening();
+    }, 10000);
+  }, []);
+
+  useEffect(() => {
+    querySearch();
+  }, [transcript]);
 
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
   }
 
+  const querySearch = () => {
+    console.log("transcript ", transcript);
+    SpeechRecognition.stopListening();
+    if (transcript === "Buscar") {
+      SpeechRecognition.startListening({ continuous: true });
+      console.log("transcript dentro de if ", transcript);
+    }
+  };
+
   return (
-    <div className='background'>
-      <div className='settings-container'>
+    <div className="background">
+      <div className="settings-container">
         <label>Uno</label>
         <select>
           <option>1</option>
@@ -32,17 +48,16 @@ const SettingsView = () => {
           <option>3</option>
         </select>
 
-        <p style={{ color: 'white' }}>Microphone: {listening ? 'on' : 'off'}</p>
-        <button onClick={SpeechRecognition.startListening}>Start</button>
+        <p style={{ color: "white" }}>Microphone: {listening ? "on" : "off"}</p>
+        {/* <button onClick={SpeechRecognition.startListening}>Start</button>
         <button onClick={SpeechRecognition.stopListening}>Stop</button>
-        <button onClick={resetTranscript}>Reset</button>
-        <p style={{ color: 'white' }}>{transcript}</p>
+        <button onClick={resetTranscript}>Reset</button> */}
+        <p style={{ color: "white" }}>{transcript}</p>
 
         <button>Guardar</button>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
 export default SettingsView;
