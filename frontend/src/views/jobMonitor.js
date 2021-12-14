82d09c8a71f9413d93154b160ac3dfb6881763db
cazabe <cazcarlos@hotmail.com>
2021-12-14 18:23:59 -0500

    New: adding axios, controllers and speech recognition logic

--- frontend/src/views/jobMonitor.js
+++ frontend/src/views/jobMonitor.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react';
-import backgroundVideo from '../assets/cyberpunk_video.mp4';
-import { GrSettingsOption } from 'react-icons/gr';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from "react";
+import backgroundVideo from "../assets/cyberpunk_video.mp4";
+import { GrSettingsOption } from "react-icons/gr";
+import { useNavigate } from "react-router-dom";
 
 const JobMonitorView = () => {
   const navigate = useNavigate();
@@ -11,7 +11,7 @@ const JobMonitorView = () => {
 
   useEffect(() => {
     isMounted.current = true;
-    return () => isMounted.current = false;
+    return () => (isMounted.current = false);
   }, []);
 
   const buildJobsRows = () => {
@@ -19,17 +19,17 @@ const JobMonitorView = () => {
 
     for (let i = 0; i < 9; i++) {
       rows.push(
-        (<div className='jobs-row' key={i}>
-          <div className='jobs-cell'>asdf</div>
-          <div className='jobs-cell'>asdf</div>
-          <div className='jobs-cell'>asdf</div>
-          <div className='jobs-cell'>asdf</div>
-          <div className='jobs-cell'>asdf</div>
-          <div className='jobs-cell'>asdffasd</div>
-          <div className='jobs-cell'>asdff</div>
-          <div className='jobs-cell'>asdf</div>
-          <div className='jobs-cell'>asdf</div>
-        </div>)
+        <div className="jobs-row" key={i}>
+          <div className="jobs-cell">asdf</div>
+          <div className="jobs-cell">asdf</div>
+          <div className="jobs-cell">asdf</div>
+          <div className="jobs-cell">asdf</div>
+          <div className="jobs-cell">asdf</div>
+          <div className="jobs-cell">asdffasd</div>
+          <div className="jobs-cell">asdff</div>
+          <div className="jobs-cell">asdf</div>
+          <div className="jobs-cell">asdf</div>
+        </div>
       );
     }
 
@@ -46,32 +46,37 @@ const JobMonitorView = () => {
         }
       }, 5000);
     }
-  }
+  };
 
   return (
     // <div onMouseMove={displayBtn}>
     <div>
       {/* <button onClick={() => connect('ws://localhost:8000', true, 5000)}>CONNECT</button> */}
       <video autoPlay loop muted>
-        <source src={backgroundVideo} type='video/mp4' />
+        <source src={backgroundVideo} type="video/mp4" />
       </video>
 
-      <div className='jobs-container'>
-        <div className='jobs-row'>
-          <div className='jobs-header'>Puesto</div>
-          <div className='jobs-header'>Compañía</div>
-          <div className='jobs-header'>Salario</div>
-          <div className='jobs-header'>Tipo</div>
-          <div className='jobs-header'>Beneficios</div>
-          <div className='jobs-header'>Remoto</div>
-          <div className='jobs-header'>Lugar</div>
+      <div className="jobs-container">
+        <div className="jobs-row">
+          <div className="jobs-header">Puesto</div>
+          <div className="jobs-header">Compañía</div>
+          <div className="jobs-header">Salario</div>
+          <div className="jobs-header">Tipo</div>
+          <div className="jobs-header">Beneficios</div>
+          <div className="jobs-header">Remoto</div>
+          <div className="jobs-header">Lugar</div>
         </div>
         {buildJobsRows()}
       </div>
 
-      {displaySettingsBtn && <GrSettingsOption className='settings-btn' onClick={() => navigate('/settings')} />}
-    </div >
-  )
-}
+      {displaySettingsBtn && (
+        <GrSettingsOption
+          className="settings-btn"
+          onClick={() => navigate("/settings")}
+        />
+      )}
+    </div>
+  );
+};
 
 export default JobMonitorView;
