be86b2d31129aaf8ef642b83a0e534240191fcf9
Bryan Calisto <bryancalisto@yahoo.es>
2021-12-13 20:41:19 -0500

    New: Added websocket server and testing client

---
+++ backend/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const WsServer = require('ws');
+const { createServer } = require('http');
+
+const app = express();
+const server = createServer(app);
+
+function initWs() {
+  const options = {
+    noServer: true
+  }
+
+  return new WsServer.Server(options);
+}
+
+function initHttpServer(port) {
+  app.get('/', (_, res) => {
+    res.send('holaaa');
+  });
+
+  server.listen(port, () => {
+    console.log(`Server is working on http://localhost:${port}`);
+  });
+
+  return app;
+}
+
+function initWebSocketServer(port = 8000) {
+  initHttpServer(port);
+  const wss = initWs();
+
+  server.on('upgrade', async (req, socket, head) => {
+    try {
+      wss.handleUpgrade(req, socket, head, (ws) => {
+        wss.emit('connection', ws, req);
+      })
+    } catch (e) {
+      console.log('Socket upgrade failed', e);
+      socket.destroy();
+    }
+  })
+
+  return wss;
+}
+
+const wss = initWebSocketServer();
+
+wss.on('connection', (ws) => {
+  ws.on('message', (data) => {
+    ws.send(data.toString());
+  });
+})
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,43 @@
+import React, { useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let keepAliveId = useRef(null);
+
+  function connect(url, keepAlive, keepAlivePeriod) {
+    try {
+      const ws = new WebSocket(url);
+
+      if (keepAlive) {
+        clearInterval(keepAliveId.current);
+        keepAliveId.current = setInterval(() => {
+          ws.send('ping');
+        }, keepAlivePeriod || 5000);
+      }
+
+      ws.onopen = () => {
+        console.log('Websocket is open');
+      }
+
+      ws.onclose = () => {
+        console.log('Websocket closed');
+      }
+
+      ws.onerror = (e) => {
+        console.log('Websocket error', e);
+      }
+
+      ws.onmessage = (message) => {
+        console.log('Websocket data', message.data);
+      }
+
+    } catch (e) {
+      console.log('Error in websocket connection: ', e);
+    }
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +45,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={() => connect('ws://localhost:8000', true, 5000)}>CONNECT</button>
         <a
           className="App-link"
           href="https://reactjs.org"
