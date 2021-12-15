22cde32cde1d07534166c23b55eae9c625cd0ae2
Bryan Calisto <bryancalisto@yahoo.es>
2021-12-14 20:42:03 -0500

    Chore: Added Search functionality

--- frontend/src/controller/JobsOffers.js
+++ frontend/src/controller/JobsOffers.js
@@ -1,18 +1,18 @@
 import api from "../utils";
 
-const getJobsOffers = async (query, page) => {
+const getJobsOffers = async (query) => {
   if (!query || query === "") {
     query = "front-end";
   }
 
   try {
     const result = await api.get(
-      `/api/v0/search/jobs?query=${query}&per_page=10&page=${page}&expand=["company"]`
+      `/api/v0/search/jobs?query=${query}&per_page=10&expand=["company"]`
     );
-    return result;
+    return result.data.data;
   } catch (e) {
     console.log("Error ", e);
-    return false;
+    return [];
   }
 };
 
--- frontend/src/views/jobMonitor.js
+++ frontend/src/views/jobMonitor.js
@@ -2,36 +2,105 @@ import React, { useEffect, useRef, useState } from "react";
 import backgroundVideo from "../assets/cyberpunk_video.mp4";
 import { GrSettingsOption } from "react-icons/gr";
 import { useNavigate } from "react-router-dom";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import getJobsOffers from "../controller/JobsOffers";
 
 const JobMonitorView = () => {
   const navigate = useNavigate();
   let keepAliveId = useRef(null);
   let isMounted = useRef(false);
+  let [toSearch, setToSearch] = useState('php');
+  let [jobsList, setJobsList] = useState([]);
   const [displaySettingsBtn, setDisplaySettingsBtn] = useState(false);
+
+  const { transcript, listening, browserSupportsSpeechRecognition, resetTranscript } =
+    useSpeechRecognition();
 
   useEffect(() => {
     isMounted.current = true;
-    return () => (isMounted.current = false);
+    SpeechRecognition.startListening({ continuous: true });
+
+    keepAliveId.current = setInterval(async () => {
+      console.log('data: ', toSearch);
+      const result = await getJobsOffers(toSearch);
+      const newJobsList = [...jobsList];
+
+      result.forEach(res => {
+        const job = {
+          url: res.links.public_url,
+          company: res.attributes.company.data.attributes.name,
+          country: res.attributes.country,
+          isRemote: res.attributes.remote ? 'Sí' : 'No',
+          perks: res.attributes.perks.join(', '),
+          minSalary: res.attributes.min_salary,
+          maxSalary: res.attributes.max_salary,
+          position: res.attributes.title,
+          type: 'type'
+        }
+
+        if (newJobsList.length <= 8) {
+          newJobsList.push(job);
+        }
+
+        console.log(res.links.public_url);
+        console.log(res.attributes.company.data.attributes.name);
+        console.log(res.attributes.remote);
+        console.log(res.attributes.country);
+        console.log(res.attributes.perks.join(', '));
+        console.log(res.attributes.min_salary);
+        console.log(res.attributes.max_salary);
+      });
+
+      console.log('jobs:', newJobsList);
+
+      setJobsList(newJobsList);
+    }, 5000);
+
+    return () => {
+      clearInterval(keepAliveId.current);
+      isMounted.current = false;
+    };
   }, []);
 
-  const buildJobsRows = () => {
-    const rows = [];
-
-    for (let i = 0; i < 9; i++) {
-      rows.push(
-        <tr key={i}>
-          <td>asdf</td>
-          <td>asdf</td>
-          <td>asdf</td>
-          <td>asdf</td>
-          <td>asdf</td>
-          <td>asdffasd</td>
-          <td>asdff</td>
-        </tr>
-      );
+  const querySearch = () => {
+    console.log("transcript ", transcript);
+    if (/borrar/.test(transcript.toLowerCase())) {
+      console.log("BORRANDO transcript dentro de if ");
+      resetTranscript();
+      setToSearch('');
+    } else if (/buscar/.test(transcript.toLowerCase())) {
+      console.log("BUSCAR transcript dentro de if ");
+      const result = /[0-9]/.exec(transcript.toLowerCase());
+      if (result) {
+        alert(jobsList[Number(result[0])].url);
+      }
+    } else {
+      setToSearch(transcript);
     }
+  };
 
-    return rows;
+  useEffect(() => {
+    querySearch();
+  }, [transcript]);
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>POR FAVOR, USE GOOGLE CHROME</span>;
+  }
+
+
+  const buildJobsRows = () => {
+    return jobsList.map((job, i) => (
+      <tr key={i}>
+        <td>{job.position}</td>
+        <td>{job.company}</td>
+        <td>{job.minSalary && job.maxSalary ? `${job.minSalary} - ${job.maxSalary}` : '???'}</td>
+        <td>{job.perks}</td>
+        <td>{job.isRemote}</td>
+        <td>{job.country}</td>
+      </tr>
+    ));
   };
 
   const displayBtn = async () => {
@@ -56,13 +125,14 @@ const JobMonitorView = () => {
 
       <table>
         <thead>
-          <th>Puesto</th>
-          <th>Compañía</th>
-          <th>Salario</th>
-          <th>Tipo</th>
-          <th>Beneficios</th>
-          <th>Remoto</th>
-          <th>Lugar</th>
+          <tr>
+            <th>Puesto</th>
+            <th>Compañía</th>
+            <th>Salario</th>
+            <th>Beneficios</th>
+            <th>Remoto</th>
+            <th>Lugar</th>
+          </tr>
         </thead>
         <tbody>{buildJobsRows()}</tbody>
       </table>
